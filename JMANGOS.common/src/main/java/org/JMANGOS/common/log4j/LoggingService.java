b31ccc9b5f648a79f5a25b06d9fe5746744bebc6
MinimaJack <minimajack@gmail.com>
2012-07-31 21:46:12 +0300

    update copyright 2

--- JMANGOS.common/src/main/java/org/JMANGOS/common/log4j/LoggingService.java
+++ JMANGOS.common/src/main/java/org/JMANGOS/common/log4j/LoggingService.java
@@ -1,16 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2011 WoWEmu
+ * Copyright (c) 2012 JMANGOS
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *    WoWEmu  - initial API and implementation
+ *     JMANGOS - initial API and implementation
  ******************************************************************************/
-/**
- * 
- */
+
 package org.JMANGOS.common.log4j;
 
 import java.io.File;
@@ -29,11 +27,10 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class LoggingService.
  *
- * @author admin
+ * @author MinimaJack
  */
 public class LoggingService implements Service
 {
--- JMANGOS.realm/src/main/java/org/JMANGOS/realm/network/crypt/SARC4.java
+++ JMANGOS.realm/src/main/java/org/JMANGOS/realm/network/crypt/SARC4.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011 WoWEmu
+ * Copyright (c) 2012 JMANGOS
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *    WoWEmu  - initial API and implementation
+ *     JMANGOS - initial API and implementation
  ******************************************************************************/
 package org.JMANGOS.realm.network.crypt;
 
