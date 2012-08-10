0873355812e7a0f78b201d4bba20ee171a3360d4
mAdloVe <madlove.storm@yahoo.com>
2012-08-11 02:43:49 +0300

    Revert "Fix pom.xml #6"
    This reverts commit fda3acbee6e56d58206cf6d0e1f2a9780fade979.

--- Commons/src/main/java/org/jmangos/commons/database/DatabaseConfig.java
+++ Commons/src/main/java/org/jmangos/commons/database/DatabaseConfig.java
@@ -24,53 +24,60 @@ import org.jmangos.commons.configuration.ConfigurableProcessor;
 import org.jmangos.commons.configuration.Property;
 import org.jmangos.commons.utils.PropertiesUtils;
 
+
+// TODO: Auto-generated Javadoc
 /**
  * This class holds all configuration of database.
- * 
+ *
  */
-public class DatabaseConfig {
-
+public class DatabaseConfig
+{
+	
 	/** Logger for database configuration. */
-	private static final Logger log = Logger.getLogger(DatabaseConfig.class);
+	private static final Logger	log	= Logger.getLogger(DatabaseConfig.class);
 
 	/** Configuration file location. */
-	public static final String CONFIG_FILE = "conf/database/database.properties";
+	public static final String	CONFIG_FILE	= "conf/database/database.properties";
 
 	/**
 	 * Default database url.
 	 */
 	@Property(key = "database.url", defaultValue = "jdbc:mysql://localhost:3306/realmd")
-	public static String DATABASE_URL = "jdbc:mysql://localhost:3306/realmd";
+	public static String		DATABASE_URL = "jdbc:mysql://localhost:3306/realmd";
 
 	/** Name of database Driver. */
 	@Property(key = "database.driver", defaultValue = "com.mysql.jdbc.Driver")
-	public static Class<?> DATABASE_DRIVER;
+	public static Class<?>		DATABASE_DRIVER;
 
 	/** Default database user. */
 	@Property(key = "database.user", defaultValue = "JMaNGOS")
-	public static String DATABASE_USER;
+	public static String		DATABASE_USER;
 
 	/** Default database password. */
 	@Property(key = "database.password", defaultValue = "JMaNGOS")
-	public static String DATABASE_PASSWORD;
+	public static String		DATABASE_PASSWORD;
 
 	/** Minimum amount of connections that are always active. */
 	@Property(key = "database.connections.min", defaultValue = "2")
-	public static int DATABASE_CONNECTIONS_MIN;
+	public static int			DATABASE_CONNECTIONS_MIN;
 
 	/** Maximum amount of connections that are allowed to use. */
 	@Property(key = "database.connections.max", defaultValue = "10")
-	public static int DATABASE_CONNECTIONS_MAX;
-
+	public static int			DATABASE_CONNECTIONS_MAX;
+	
 	/**
 	 * Loads database configuration.
 	 */
-	public static void load() {
+	public static void load()
+	{
 
 		Properties p;
-		try {
+		try
+		{
 			p = PropertiesUtils.load(CONFIG_FILE);
-		} catch (IOException e) {
+		}
+		catch(IOException e)
+		{
 			log.fatal("Can't load database configuration...");
 			throw new Error("Can't load " + CONFIG_FILE, e);
 		}
