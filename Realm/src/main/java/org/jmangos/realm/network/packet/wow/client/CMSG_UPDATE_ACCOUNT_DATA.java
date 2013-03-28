d99f22a50f237f2b18a3bcd1ac82bcf073f93650
MinimaJack <MinimaJack@gmail.com>
2013-03-28 12:37:29 +0200

    Typo CMSG_UPDATE_ACCOUNT_DATA

---
+++ Realm/src/main/java/org/jmangos/realm/network/packet/wow/client/CMSG_UPDATE_ACCOUNT_DATA.java
@@ -0,0 +1,40 @@
+package org.jmangos.realm.network.packet.wow.client;
+
+import java.nio.BufferUnderflowException;
+
+import org.jmangos.commons.network.sender.AbstractPacketSender;
+import org.jmangos.realm.network.packet.wow.AbstractWoWClientPacket;
+import org.jmangos.realm.network.packet.wow.server.SMSG_LOGOUT_COMPLETE;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+/**
+ * 
+ * @author MinimaJack
+ *
+ */
+@Component
+public class CMSG_UPDATE_ACCOUNT_DATA extends AbstractWoWClientPacket {
+
+    Logger log = LoggerFactory.getLogger(CMSG_UPDATE_ACCOUNT_DATA.class);
+    /** The sender. */
+    @Autowired
+    @Qualifier("nettyPacketSender")
+    private AbstractPacketSender sender;
+
+    @Override
+    protected void readImpl() throws BufferUnderflowException, RuntimeException {
+
+        int type,timestamp,decompressedSize;
+        readD();
+        readD();
+        decompressedSize = readD();
+    }
+
+    @Override
+    protected void runImpl() {
+
+    }
+}
