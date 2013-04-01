8bc7902288485cba2b75e4f4468af9b952b65c6a
MinimaJack <MinimaJack@gmail.com>
2013-04-01 11:22:57 +0300

    Update copyright.

--- Tools/src/main/java/org/jmangos/tools/openGL/IconLoader.java
+++ Tools/src/main/java/org/jmangos/tools/openGL/IconLoader.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (C) 2012 JMaNGOS <http://jmangos.org/>
- * 
+ * Copyright (C) 2013 JMaNGOS <http://jmangos.org/>
+ *  
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 2 of the License, or (at your
@@ -13,7 +13,7 @@
  * 
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
+ ******************************************************************************/
 package org.jmangos.tools.openGL;
 
 import java.awt.Graphics2D;
